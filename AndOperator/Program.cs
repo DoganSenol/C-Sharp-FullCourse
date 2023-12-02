2c0b800814d63d9e21762f46de7f13979f939748
DoganSenol <senoldogan2018@gmail.com>
2023-12-02 17:41:57 +0100

    Add project files.

---
+++ AndOperator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // && and Operator
+
+            int number1 = 5;
+
+            int number2 = 5;
+
+            int number3 = 10;
+
+            bool control = number1 == number2 && number1 < number3; // True
+
+            Console.WriteLine("Control="+ control);
+            Console.ReadLine();
+
+        }
+    }
+}
