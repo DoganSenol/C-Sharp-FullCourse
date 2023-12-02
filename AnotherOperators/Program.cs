b6667f33a97b74b2ed719bd03f15219e7676c030
DoganSenol <senoldogan2018@gmail.com>
2023-12-02 17:46:14 +0100

    Update

---
+++ AnotherOperators/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // +=
+            // -=
+            // /=
+            // *=
+
+            int a = 6;
+            int b = 5;
+            int c = 8;
+            int d = 2;
+
+
+
+
+             // first solution
+            // a = a + 4;
+            //second solution
+            a += 4;
+
+            // first solution
+            // b = b - 4;
+            //second solution
+            b -= 4;
+
+            // first solution
+            // c = c / 4;
+            //second solution
+            c /= 4;
+
+            // first solution
+            // d = d * 4;
+            //second solution
+            d *= 4;
+
+            Console.WriteLine("Value of a="  +a);
+            Console.WriteLine("Value of b=" + b);
+            Console.WriteLine("Value of c=" + c);
+            Console.WriteLine("Value of d=" + d);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
