547e3df7393c5307ade6ba66d6cabbbecd65bb30
DoganSenol <senoldogan2018@gmail.com>
2023-12-02 18:19:39 +0100

    update

---
+++ AssignmentOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //assignment operator : =
+
+            int age = 21;
+
+            Console.WriteLine("Age="+age);
+            Console.ReadLine();
+        }
+    }
+}
