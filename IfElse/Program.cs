3dc1dfeade519aaebc667d77f4c071a8093785af
DoganSenol <senoldogan2018@gmail.com>
2023-12-03 10:48:56 +0100

    IfElse Completed

--- IfElse/Program.cs
+++ IfElse/Program.cs
@@ -10,6 +10,22 @@ namespace IfElse
     {
         static void Main(string[] args)
         {
+            //Write code that checks if the student's grade is above 50. If it is, print successful; if not, print unsuccessful
+
+            int grade = 75;
+            
+            if(grade>50)
+            {
+                Console.WriteLine("Successful");
+            }
+
+        else
+            {
+                Console.WriteLine("Unsuccesful");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
