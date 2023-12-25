e421418ff73ca99ec84fe6cb7a3d35b74829fcc5
DoganSenol <senoldogan2018@gmail.com>
2023-12-25 23:20:26 +0100

    For Loop Excercises

---
+++ ForLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1 den 10 a kadar olan sayilari alt alta ekrana yazdiran programi kodlayiniz
+            // 1 den 10 a kadar olan sayilardan cift olanlarini ekrana yazdiriniz
+
+            /*
+            for(int i = 1; i<=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            */
+
+            for(int a =0; a<=10; a=a+2)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
