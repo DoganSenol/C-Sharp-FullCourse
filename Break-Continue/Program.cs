b9c7e57ba9bbe60843091ec0cc084818f13e90ad
DoganSenol <senoldogan2018@gmail.com>
2024-01-02 19:38:26 +0100

    Break & Continue

---
+++ Break-Continue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // break anahtar kelimesi
+            // continue anahtar kelimesi
+
+            /*
+            for (int a = 1; a <= 10; a++)
+            {
+                if(a==7)
+                    {
+                    break;
+
+                }
+                Console.WriteLine("a ' nin degeri :"+a);
+            }
+            Console.ReadLine();
+            */
+
+            for (int a = 1; a <= 10; a++)
+            {
+                if (a == 7)
+                {
+                    continue;
+
+                }
+                Console.WriteLine("a ' nin degeri :" + a);
+            }
+            Console.ReadLine();
+        }
+    }
+    }
+
